584ab0da63fa74af4d993bc05e457270f45f467d
nacin <nacin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-31 19:41:50 +0000

    Filtered HTML for Editors: Use is_super_admin() instead, for forward compat reasons. Noop if multisite for reasons specified.Less clever, but that's what I get for being a smartass.
    git-svn-id: https://plugins.svn.wordpress.org/filtered-html-for-editors/trunk@327618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- filtered-html-for-editors.php
+++ filtered-html-for-editors.php
@@ -9,20 +9,27 @@
 /*
  * Plugin Name: Filtered HTML for Editors
  * Plugin URI: http://wordpress.org/extend/plugins/filtered-html-for-editors/
- * Description: Prevents editors from publishing content with unfiltered HTML. By default, editors and administrators are given this ability. See <a href="http://codex.wordpress.org/FAQ_Security#Why_are_some_users_allowed_to_post_unfiltered_HTML.3F">this page on the Codex</a> for reasons why. This plugin has no effect on multisite as unfiltered HTML is only given to super administrators.
+ * Description: Prevents editors from publishing content with unfiltered HTML. By default, editors and administrators are given this ability. See <a href="http://codex.wordpress.org/FAQ_Security#Why_are_some_users_allowed_to_post_unfiltered_HTML.3F">this page on the Codex</a> for reasons. This plugin does not work on multisite, as unfiltered HTML is by default only given to super administrators in that case.
  * Author: Andrew Nacin
  * Author URI: http://andrewnacin.com/
  * Version: 1.0
  * License: GPL
  */
 
+/*
+ * If we're running multisite, we don't want to do anything. By default, only
+ * super admins will have unfiltered HTML, which is what this code does anyway.
+ * The reason why we don't want to pile on top of that, is that it'll break
+ * the Unfiltered MU plugin if it's in use. So it's safer to noop.
+ */
+if ( is_multisite() )
+	return;
+
 /**
  * Denies unfiltered HTML for Editors.
  *
- * Works by also requiring the user to possess the delete_users capability whenever
- * the unfiltered_html capability is requested. Only administrators have the
- * delete_users capability, and it is how we establish whether you are a 'super admin'
- * when not running multisite.
+ * Works by using the is_super_admin() function. On single site, this returns
+ * true for users with the delete_users cap, which only administrators have.
  *
  * This function is attached to the map_meta_cap filter.
  *
@@ -30,9 +37,9 @@
  * @param $cap The requested capability passed to map_meta_cap.
  * @return array The new array of mapped capabilities.
  */
-function nacin_filtered_html_for_editors( $caps, $cap ) {
-	if ( 'unfiltered_html' == $cap )
-		$caps[] = 'delete_users';
+function nacin_filtered_html_for_editors( $caps, $cap, $user_id ) {
+	if ( 'unfiltered_html' == $cap && ! is_super_admin( $user_id ) )
+		$caps[] = 'do_not_allow';
 	return $caps;
 }
-add_filter( 'map_meta_cap', 'nacin_filtered_html_for_editors', 10, 2 );
+add_filter( 'map_meta_cap', 'nacin_filtered_html_for_editors', 10, 3 );
